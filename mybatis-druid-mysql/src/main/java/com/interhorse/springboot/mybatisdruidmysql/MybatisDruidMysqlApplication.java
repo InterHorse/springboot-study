605b0b9407634732d4f84cb377d7db2461ed936e
InterHorse <mayuzhe1222@qq.com>
2020-06-03 17:02:05 +0800

    feat: 添加 mybatis-druid-mysql 项目

---
+++ mybatis-druid-mysql/src/main/java/com/interhorse/springboot/mybatisdruidmysql/MybatisDruidMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.interhorse.springboot.mybatisdruidmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDruidMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisDruidMysqlApplication.class, args);
+	}
+
+}
