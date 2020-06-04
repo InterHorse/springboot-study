b160c35ba00215b16b3ff57ec7f8ef655ca4ddc3
InterHorse <mayuzhe1222@qq.com>
2020-06-04 19:57:55 +0800

    fix: 修改包名

---
+++ my-first-springboot/src/main/java/cn/interhorse/springboot/myfirstspringboot/MyFirstSpringbootApplication.java
@@ -0,0 +1,12 @@
+package cn.interhorse.springboot.myfirstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringbootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringbootApplication.class, args);
+	}
+
+}
