cc143280a5582d570d5c822be27da38a02b6b662
InterHorse <mayuzhe1222@qq.com>
2020-07-03 16:45:20 +0800

    style: 去掉无用代码

--- custom-configuration/src/main/java/cn/interhorse/springboot/customconfiguration/CustomConfigurationApplication.java
+++ custom-configuration/src/main/java/cn/interhorse/springboot/customconfiguration/CustomConfigurationApplication.java
@@ -2,8 +2,6 @@ package cn.interhorse.springboot.customconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class CustomConfigurationApplication {
